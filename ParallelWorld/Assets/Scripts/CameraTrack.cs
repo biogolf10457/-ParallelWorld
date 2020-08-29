3eb3134a1ce3e8d43aab3e9c3c7ff0faf7c5f48a
biogolf10457 <69182007+biogolf10457@users.noreply.github.com>
2020-08-29 13:23:49 +0700

    loop background(not finish) and use item

--- ParallelWorld/Assets/Scripts/CameraTrack.cs
+++ ParallelWorld/Assets/Scripts/CameraTrack.cs
@@ -1,15 +1,72 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class CameraTrack : MonoBehaviour
 {
     public GameObject Player;
     public float adjustY;
+    private float initX;
+    private float initY;
+    public GameObject[] levels;
+    private Camera mainCamera;
+    private Vector2 screenBounds;
+    public float choke;
+    public float scrollSpeed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //set start position of camera
+        initX = Player.transform.position.x; 
+        initY = Player.transform.position.y;
+
+
+        //set background buffer
+        mainCamera = gameObject.GetComponent<Camera>();
+        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
+        foreach (GameObject obj in levels)
+        {
+            loadChildObjects(obj);
+        }
+    }
+
+    void loadChildObjects(GameObject obj)
+    {
+        float objectHeight = obj.GetComponent<SpriteRenderer>().bounds.size.y - choke;
+        int childsNeeded = (int)Mathf.Ceil(screenBounds.y * 2 / objectHeight);
+        UnityEngine.Debug.Log(childsNeeded);
+        GameObject clone = Instantiate(obj) as GameObject;
+        for (int i = 0; i <= childsNeeded; i++)
+        {
+            GameObject c = Instantiate(clone) as GameObject;
+            c.transform.SetParent(obj.transform);
+            c.transform.position = new Vector3(obj.transform.position.x, objectHeight * i, obj.transform.position.z);
+            c.name = obj.name + i;
+        }
+        Destroy(clone);
+        Destroy(obj.GetComponent<SpriteRenderer>());
+    }
+
+    void repositionChildObjects(GameObject obj)
+    {
+        Transform[] children = obj.GetComponentsInChildren<Transform>();
+        if (children.Length > 1)
+        {
+            GameObject firstChild = children[1].gameObject;
+            GameObject lastChild = children[children.Length - 1].gameObject;
+            float halfObjectHeight = lastChild.GetComponent<SpriteRenderer>().bounds.extents.y - choke;
+            if (transform.position.y + screenBounds.y > lastChild.transform.position.y + halfObjectHeight)
+            {
+                firstChild.transform.SetAsLastSibling();
+                firstChild.transform.position = new Vector3(lastChild.transform.position.x, lastChild.transform.position.y + halfObjectHeight * 2, lastChild.transform.position.z);
+            }
+            else if (transform.position.y - screenBounds.y < firstChild.transform.position.y - halfObjectHeight)
+            {
+                lastChild.transform.SetAsFirstSibling();
+                lastChild.transform.position = new Vector3(firstChild.transform.position.x, firstChild.transform.position.y - halfObjectHeight * 2, firstChild.transform.position.z);
+            }
+        }
     }
 
     // Update is called once per frame
@@ -17,7 +74,15 @@ public class CameraTrack : MonoBehaviour
     {
         if (Player != null)
         {
-            transform.position = new Vector3(Player.transform.position.x, Player.transform.position.y + adjustY, -10);
+            transform.position = new Vector3(initX, Player.transform.position.y + adjustY, -10);
+        }
+    }
+
+    void LateUpdate()
+    {
+        foreach (GameObject obj in levels)
+        {
+            repositionChildObjects(obj);
         }
     }
 }
--- ParallelWorld/Assets/Scripts/Player.cs
+++ ParallelWorld/Assets/Scripts/Player.cs
@@ -10,17 +10,43 @@ public class Player : MonoBehaviour
     private BoxCollider2D boxCollider2d;
     public KeyCode left;
     public KeyCode right;
-    public float realSpeedX;
+    public KeyCode useItem;
+    private float realSpeedX;
+    private float realSpeedY;
+    private string items;
+    public GameObject rival;
+    private Player rivalScript;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         boxCollider2d = GetComponent<BoxCollider2D>();
+        realSpeedY = speedY;
+        items = "";
+        rivalScript = rival.GetComponent<Player>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+
+
+        //use item
+        if (Input.GetKey(useItem) && string.Compare(items, "") != 0)
+        {
+            if(string.Compare(items, "frog") == 0)
+            {
+                rivalScript.slow();
+            }
+            if (string.Compare(items, "shield") == 0)
+            {
+                //rival.slow();
+            }
+            items = "";
+        }
+
+        //control movement
         if (Input.GetKey(left))
         {
             realSpeedX = -speedX * Time.deltaTime;
@@ -33,6 +59,38 @@ public class Player : MonoBehaviour
         {
             realSpeedX = 0;
         }
-        rb.velocity = new Vector2(realSpeedX, speedY * Time.deltaTime);
+        rb.velocity = new Vector2(realSpeedX, realSpeedY * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Item"))
+        {
+            Destroy(collision.gameObject);
+            randomItem();
+        }
+    }
+
+    private void randomItem()
+    {
+        if (this.transform.position.y >= rival.transform.position.y)
+        {
+            items = "shield";
+        }
+        else
+        {
+            items = "frog";
+        }
+        UnityEngine.Debug.Log(items);
+    }
+
+    private void setDefault()
+    {
+        realSpeedY = speedY;
+    }
+
+    public void slow()
+    {
+        realSpeedY = realSpeedY / 2;
     }
 }
