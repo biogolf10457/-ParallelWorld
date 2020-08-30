dfffd02a0958e9a824a0df6b1ceb9dff79266078
biogolf10457 <69182007+biogolf10457@users.noreply.github.com>
2020-08-30 12:09:14 +0700

    random number for creating item

--- ParallelWorld/Assets/Scripts/ItemManager.cs
+++ ParallelWorld/Assets/Scripts/ItemManager.cs
@@ -6,10 +6,12 @@ public class ItemManager : MonoBehaviour
 {
     private int childrenNum;
     public GameObject player;
+    private string[] itemList;
     // Start is called before the first frame update
     void Start()
     {
         childrenNum = gameObject.transform.childCount;
+        itemList = new string[]{"shield","potion","vision","swap","hex","trap","power","amulet","counter","random"};
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class ItemManager : MonoBehaviour
             GameObject child = gameObject.transform.GetChild(i).gameObject;
             if (child.transform.position.y < player.transform.position.y + 20 && !child.GetComponent<Item>().getIsCreated())
             {
+                int randomNum = Random.Range(0, 100);
                 child.GetComponent<Item>().setName("frog");
             }
         }
